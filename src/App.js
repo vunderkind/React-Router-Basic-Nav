d73a4009b17cbab98837142dca33da1cd237c467
vunderkind <justin.irabor@gmail.com>
2019-09-17 04:08:21 +0100

    Added the BrowserRouter as Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+
+<Route path="/" exact component={Index} />
+<Route path="/about/" component={About} />
+<Route path="/users/" component={Users} />
 
 const App = () => (
   <div>
